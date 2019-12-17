5a50de47870526cc515c5b288237e83d67e98a45
Assyl-Salah <salaha@student.mini.pw.edu.pl>
2019-12-17 20:26:39 +0100

    LABTask10

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,1 +1,2 @@
-export const EMPLOYEES_LOADED = 'Employees/EMPLOYEES_LOADED';
+// eslint-disable-next-line import/prefer-default-export
+export const EMPLOYEES_LOADED = "Employees/EMPLOYEES_LOADED";
